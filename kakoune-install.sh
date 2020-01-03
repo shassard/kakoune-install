8cf4d0d16677cd4c0300031c6c0b33a6e07f2e6e
Stephen Hassard <steve@hassard.net>
2020-01-02 16:31:36 -0800

    First go

---
+++ kakoune-install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -ex
+
+# kakoune
+#sudo apt install libncursesw5-dev pkg-config
+#git clone https://github.com/mawww/kakoune.git
+#PREFIX=$HOME/.local make -C kakoune/src install
+
+# kak-lsp
+curl -L https://github.com/ul/kak-lsp/releases/download/v7.0.0/kak-lsp-v7.0.0-x86_64-unknown-linux-musl.tar.gz | tar -xz
+mkdir -p $HOME/.local/bin
+mv kak-lsp $HOME/.local/bin/
+mkdir -p $HOME/.config/kak-lsp
+sed "s/rls/ra_lsp_server/g" kak-lsp.toml > kak-lsp-new.toml
+mv kak-lsp-new.toml $HOME/.config/kak-lsp/
+
+# rust-analyzer
+git clone https://github.com/rust-analyzer/rust-analyzer
+pushd .
+cd rust-analyzer
+cargo xtask install --server
+popd
+
+# kakrc
+mkdir -p $HOME/.config/kak
+cat > $HOME/.config/kak/kakrc << EOF
+eval %sh{kak-lsp --kakoune -s \$kak_session}
+lsp-enable
+set-option global lsp_server_configuration rust.clippy_preference="on"
+add-highlighter global/ number-lines -relative
+add-highlighter global/ show-matching
+EOF
+
+# autoloads
+mkdir -p $HOME/.config/kak/autoload
+if [ ! -L $HOME/.config/kak/autoload/kak ]; then
+ echo creating autoload symlink
+ ln -s $HOME/.local/share/kak $HOME/.config/kak/autoload/kak
+fi
+curl -o $HOME/.config/kak/autoload/cargo.kak https://gitlab.com/Screwtapello/kakoune-cargo/raw/master/cargo.kak 
