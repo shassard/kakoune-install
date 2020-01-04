b94c0c4fedb8805602a3307b70c88e254c6b662c
Stephen Hassard <steve@hassard.net>
2020-01-03 20:20:22 -0800

    Use cargo for rust builds

--- kakoune-install.sh
+++ kakoune-install.sh
@@ -5,14 +5,12 @@ set -ex
 # kakoune
 sudo apt install libncursesw5-dev pkg-config
 git clone https://github.com/mawww/kakoune.git
-PREFIX="$HOME"/.local nice make -j12 -C kakoune/src install
+PREFIX="$HOME"/.local make -j12 -C kakoune/src install
 
 # kak-lsp
-curl -L https://github.com/ul/kak-lsp/releases/download/v7.0.0/kak-lsp-v7.0.0-x86_64-unknown-linux-musl.tar.gz | tar -xz
-mkdir -p "$HOME"/.local/bin
-cp kak-lsp "$HOME"/.local/bin/
+cargo install --git https://github.com/ul/kak-lsp
 mkdir -p "$HOME"/.config/kak-lsp
-sed "s/rls/ra_lsp_server/g" kak-lsp.toml > "$HOME"/.config/kak-lsp/kak-lsp.toml
+curl -L https://raw.githubusercontent.com/ul/kak-lsp/master/kak-lsp.toml | sed "s/\"rls\"/\"ra_lsp_server\"/g" > "$HOME"/.config/kak-lsp/kak-lsp.toml
 
 # clippy
 rustup component add clippy
@@ -21,21 +19,23 @@ rustup component add clippy
 rustup component add rls rust-analysis rust-src
 
 # rust-analyzer
-git clone https://github.com/rust-analyzer/rust-analyzer
-pushd .
-cd rust-analyzer
-nice cargo xtask install --server
-popd
+cargo install --git https://github.com/rust-analyzer/rust-analyzer ra_lsp_server
 
 # kakrc
 mkdir -p "$HOME"/.config/kak
 cat > "$HOME"/.config/kak/kakrc << EOF
+set-option global ui_options ncurses_assistant=off
+set-option global tabstop     4
+set-option global indentwidth 4
+add-highlighter global/ number-lines -relative -hlcursor
+add-highlighter global/ show-matching
+add-highlighter global/ regex '\h+$' 0:Error
+
+colorscheme gruvbox
+
 eval %sh{kak-lsp --kakoune -s \$kak_session}
 lsp-enable
 set-option global lsp_server_configuration rust.clippy_preference="on"
-set global ui_options ncurses_assistant=off
-add-highlighter global/ number-lines -relative
-add-highlighter global/ show-matching
 EOF
 
 # autoloads
