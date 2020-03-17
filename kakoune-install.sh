7e7a6fc980f55252dba1b9c587696b3f770939d8
Stephen Hassard <2355041+shassard@users.noreply.github.com>
2020-03-17 11:33:33 -0700

    sync kakoune config

--- kakoune-install.sh
+++ kakoune-install.sh
@@ -50,25 +50,28 @@ mkdir -p "$HOME"/.config/kak
 cat > "$HOME"/.config/kak/kakrc << EOF
 source "%val{config}/plugins/plug.kak/rc/plug.kak"
 set-option global plug_always_ensure true
-set-option global ui_options ncurses_assistant=off
-#set-option global tabstop 4
-set-option global indentwidth 4
-add-highlighter global/ number-lines -relative -hlcursor
+set-option global ui_options ncurses_assistant=off ncurses_set_title=false
+
+add-highlighter global/ number-lines #-hlcursor
 add-highlighter global/ show-matching
-add-highlighter global/ regex '\h+$' 0:Error
+add-highlighter global/ regex '\h+$' 0:Error # show trailing whitespace as an error
+
 colorscheme gruvbox
-eval %sh{kak-lsp --kakoune -s \$kak_session}
-hook global WinSetOption filetype=(rust|python|go) %{
-	lsp-enable-window
-}
+
+eval %sh{kak-lsp --kakoune -s $kak_session}
+set-option global lsp_server_configuration rust.clippy_preference=on
+map global user l ':enter-user-mode lsp<ret>' -docstring 'enter lsp user mode'
+hook global WinSetOption filetype=(rust|python|go) %{lsp-enable-window}
+
 set-face global DiagnosticError default+u
 set-face global DiagnosticWarning default+u
-set-option global lsp_server_configuration rust.clippy_preference="on"
-map global user l ':enter-user-mode lsp<ret>' -docstring 'enter lsp user mode'
-plug "lePerdu/kakboard" ensure config %{
-    hook global WinCreate .* %{ kakboard-enable }
-}
-hook global WinCreate .* %{ git show-diff }
+
+plug "lePerdu/kakboard" config %{hook global WinCreate .* %{kakboard-enable}}
+
+hook global WinCreate ^[^*]+$ %{git show-diff}
+hook global BufWritePost ^[^*]+$ %{git show-diff}
+
+hook global WinCreate ^[^*]+$ %{editorconfig-load}
 EOF
 
 # autoloads
